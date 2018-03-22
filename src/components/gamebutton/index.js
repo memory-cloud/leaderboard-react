9d74b7bdbd270282095f208b61748e254bd0bdd6
Rafael Santos <rafaelnsantos1@gmail.com>
2018-03-22 16:11:51 -0300

    initial commit

---
+++ src/components/gamebutton/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default ({leaderboard, appid, name}) => {
+  let url = '#/?key=' + leaderboard + '&appid=' + appid + '&name=' + name
+  return (
+    <div>
+        <a href={url}>{name} ({leaderboard})</a>
+    </div>
+  );
+};
