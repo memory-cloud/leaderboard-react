dd29a529aabfaddafa7a4485d2379920c5202a25
Rafael Santos <rafaelnsantos1@gmail.com>
2018-03-23 13:14:55 -0300

    https

--- src/components/entry/index.js
+++ src/components/entry/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default ({id, score}) => {
-  let url = 'http://graph.facebook.com/' + id + '/picture?type=square'
+  let url = 'https://graph.facebook.com/' + id + '/picture?type=square'
   return (
     <div>
       <img src={url} alt=''/>
